4846d7b62aec8f4abf70776ba2bcea51be0ea1eb
Charlie Slater <charliemslater1701@gmail.com>
2021-07-21 11:16:24 +0100

    Connected to mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-const app = express();
-
-app.use(express.json());
-
 mongoose.connect(
-  process.env.DB_HOST,
+  process.env.DB_URI,
   { useUnifiedTopology: true, useNewUrlParser: true },
-  () => console.log(`Connected! Listening on port ${process.env.DB_HOST}`)
+  () => console.log(`Connected to DB`)
 );
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
