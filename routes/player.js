d1b63084cbbeb55d0f84c1c91493f9977e929d96
Charlie Slater <charliemslater1701@gmail.com>
2021-07-21 12:11:02 +0100

    Added player post route

--- routes/player.js
+++ routes/player.js
@@ -1,11 +1,24 @@
 var express = require("express");
 var router = express.Router();
-const Players = require("../models/Player");
+const Player = require("../models/Player");
 
 router.get("/", async (req, res) => {
   try {
-    const players = await Players.find({});
+    const players = await Player.find({});
     res.json(players);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const player = new Player({
+    name: req.body.name,
+    score: req.body.score,
+  });
+  try {
+    const savePlayer = await player.save();
+    res.json(savePlayer);
   } catch (err) {
     res.json({ message: err });
   }
